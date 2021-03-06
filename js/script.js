60e90354d8930ef0886010db25e74a5c1da38d65
Suryanshpsurya <suryanshpsurya@gmail.com>
2021-03-06 12:30:42 +0530

    upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*navgation open*/
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/*navgation closed*/
+/*contact form start*/
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+/*contact form end*/
