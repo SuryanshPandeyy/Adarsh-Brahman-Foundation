aa957eade3664ad7869db202241877b1e390d92e
Suryansh Pandey <suryanshpsurya@gmail.com>
2021-08-07 14:49:28 +0530

    update

--- js/script.js
+++ js/script.js
@@ -7,23 +7,9 @@ $(".close").on("click", () => {
 })
 /*navgation closed*/
 
-/*contact form start*/
-// Get the modal
-var modal1 = document.getElementById('id01');
-var blocker1 = document.getElementById('blocker1');
-
-// When the user clicks anywhere outside of the modal, close it
-blocker1.onclick = function () {
-        modal1.style.display = "none";
-} 
-/*contact form end*/
 /* join start*/
-var modal2 = document.getElementById('id02');
-var blocker2 = document.getElementById('blocker2');
+$("#blocker2").on("click", () => {
+    $("#id02").css("display", "none");
+})
 
-// When the user clicks anywhere outside of the modal, close it
-blocker2.onclick = function () {
-        modal2.style.display = "none";
-}
 /* join end*/
-
